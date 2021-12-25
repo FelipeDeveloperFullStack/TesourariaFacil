4e19a89ea5efb06f88c4f54b213ea3877ae08641
Felipe Miguel dos Santos <felipe.msantos@tagbss.com>
2021-12-25 02:16:29 -0300

    Remove some imports not used

--- src/app/components/toolbar-top/index.tsx
+++ src/app/components/toolbar-top/index.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import HomeWorkIcon from '@material-ui/icons/HomeWork';
 import { useStyles, AppBarStyled } from './styles'
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
